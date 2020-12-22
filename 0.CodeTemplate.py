1954c11ab881dc38404e26f77c85d5adb8a1ba6a
Vu Nguyen <vu.nguyenq89@gmail.com>
2020-12-22 13:56:18 +0100

    Added answer format and time tracking to template

--- 0.CodeTemplate.py
+++ 0.CodeTemplate.py
@@ -1,3 +1,4 @@
+import datetime
 input_raw = '''
 '''
 
@@ -12,5 +13,24 @@ input_test = input_test_raw.split("\n")
 input = input_test
 
 # PART 1
+# Time tracking
+p1_start_time = datetime.datetime.now() 
+
+
+answer = 0
+
+
+print('-'*10 + 'PART 1' + '-'*10)
+print('Part 1 answer: {}'.format(answer))
+p1_end_time = datetime.datetime.now() 
+print('Part 1 time: {}'.format(p1_end_time - p1_start_time))
 
 # PART 2
+# Time tracking
+p2_start_time = datetime.datetime.now()
+
+
+print('-'*10 + 'PART 2' + '-'*10)
+print('Part 2 answer: {}'.format(answer))
+p2_end_time = datetime.datetime.now() 
+print('Part 2 time: {}'.format(p2_end_time - p2_start_time))
